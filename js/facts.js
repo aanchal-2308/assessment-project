842365efbeb7d5ec954342660d44646642e97a1a
aanchal-2308 <52851861+aanchal-2308@users.noreply.github.com>
2019-11-22 13:41:40 +0530

    Update facts.js

--- js/facts.js
+++ js/facts.js
@@ -1,6 +1,6 @@
 $(document).on('click',"#getbtn",function(){
     if($('#getnum').val()){
-       var url='http://numbersapi.com/'+$('#getnum').val()+'?default=Boring+number+is+boring.';
+       var url='https://numbersapi.com/'+$('#getnum').val()+'?default=Boring+number+is+boring.';
         console.log(url);
      $.get(url,function(data){
     console.log(data);
